4cacb5f880276c06b52e1d288b2c0329701d8a1f
ea7a024f03bf <r.mustapha@alustudent.com>
2023-07-04 11:52:43 -0700

    loops

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(1, 10):
+for i in range(0, 8):
     for j in range(i + 1, 10):
-        print("{:02d}, {:02d}".format(i, j), end=", ")
-print("\n")
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
