ae830a5969d2320b10c10e8ecc3eec9ce9c4e0a3
ea7a024f03bf <r.mustapha@alustudent.com>
2023-07-05 00:20:39 -0700

    import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-
-from add_0 import add
-
-result = add(a, b)
-
-print("{:d} + {:d} = {:d}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
