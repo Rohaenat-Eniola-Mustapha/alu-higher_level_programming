2eaf33cf3d9f2ffb486ec8557833abd140711ff4
ea7a024f03bf <r.mustapha@alustudent.com>
2023-07-04 23:43:29 -0700

    loops amd comdoitions

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         print("{:d}".format(-(number % -10)), end='')
-        return(-(number % 10))
+        return(-(number % -10))
     else:
         print("{:d}".format(number % 10), end='')
         return(number % 10)
