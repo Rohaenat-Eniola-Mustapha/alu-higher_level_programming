bed182db2ceb24c796658a67dfd91040dcbcbb4f
ea7a024f03bf <r.mustapha@alustudent.com>
2023-07-06 04:45:03 -0700

    advanced tasks

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
+
 import sys
 from calculator_1 import add, subtract, multiply, divide
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-result = None
+    result = None
 
-if operator == "+":
-    result = add(a, b)
-elif operator == "-":
-    result = subtract(a, b)
-elif operator == "*":
-    result = multiply(a, b)
-elif operator == "/":
-    result = divide(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = subtract(a, b)
+    elif operator == "*":
+        result = multiply(a, b)
+    elif operator == "/":
+        result = divide(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-print(f"{a} {operator} {b} = {result}")
+    print(f"{a} {operator} {b} = {result}")
