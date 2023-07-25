ff222e0394fc047c71a2ed9557a7e2a97c720413
ea7a024f03bf <r.mustapha@alustudent.com>
2023-07-25 11:13:38 -0700

    task

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""A module"""
 
 
 class Student:
-    """Represent a student."""
-
+    """calling a class"""
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """This is an init function"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """This is an init function"""
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+    def reload_from_json(self, json):
+        """This is an init function"""
+        for key, value in json.items():
+            setattr(self, key, value)
