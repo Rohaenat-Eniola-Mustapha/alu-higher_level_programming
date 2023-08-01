30836ccb14d0f4d22b498e7886653dac3f8cddc6
ea7a024f03bf <r.mustapha@alustudent.com>
2023-08-01 10:43:18 -0700

    creates

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- create table second_table and add multiple rows
+CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
+INSERT second_table VALUES (1,'John',10), (2,'Alex',3), (3,'Bob',14), (4,'George',8);
