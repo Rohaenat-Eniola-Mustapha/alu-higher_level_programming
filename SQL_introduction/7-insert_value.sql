38fda4d60e763fb037a3f8b3611f904c5104c191
ea7a024f03bf <r.mustapha@alustudent.com>
2023-08-01 07:15:12 -0700

    insert

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new table in the first_table in MySQL
-INSERT INTO 'first_table' ('id', 'name'), VALUES(89, 'BEST SCHOOL');
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
