21968739f6a3998f54648f7a6b502de927cc8484
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-08-04 07:42:08 +0000

    urillb

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ url = 'https://alu-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     body_response = response.read()
     body_type = type(body_response)
-    body_content = b'Custom status'
-    body_utf8_content = 'Custom status'
+    body_content = body_response
+    body_utf8_content = body_response.decode('utf-8')
 
 print("Body response:")
 print("\t- type:", body_type)
