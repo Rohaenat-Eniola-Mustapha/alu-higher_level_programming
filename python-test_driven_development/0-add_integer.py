f68b0a83874154a4551b36a55240261daeb73587
ea7a024f03bf <r.mustapha@alustudent.com>
2023-08-05 14:15:31 -0700

    integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' write a function that adds 2 integers'''
+
+def add_integer(a, b=98):
+    '''a function that carries two arguements a and b'''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
