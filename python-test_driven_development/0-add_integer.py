f59e9f470bf0b8a98173a5e94e11ac9cb92e4151
ea7a024f03bf <r.mustapha@alustudent.com>
2023-08-06 10:19:51 -0700

    new

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' write a function that adds 2 integers'''
 
+
 def add_integer(a, b=98):
     '''a function that carries two arguements a and b'''
     if not isinstance(a, (int, float)):
