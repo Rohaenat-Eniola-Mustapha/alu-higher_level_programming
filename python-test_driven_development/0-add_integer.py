b177dea4dbb2361a4f4a31a3400a447f518b5c2b
ea7a024f03bf <r.mustapha@alustudent.com>
2023-08-06 10:16:50 -0700

    new

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,10 @@
 def add_integer(a, b=98):
     '''a function that carries two arguements a and b'''
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer")
 
     a = int(a)
     b = int(b)
