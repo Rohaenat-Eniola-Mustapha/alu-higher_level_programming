163149b9474cb496a74dd843bd28b393a05ff712
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-09-26 10:34:08 -0700

    number 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-
-// Get the first argument and convert it to an integer
 const x = parseInt(process.argv[2]);
 
-// Check if the conversion was successful and x is a positive integer
-if (!isNaN(x) && x > 0) {
-  // Loop x times and print "C is fun" each time
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else {
-  // Print " " if the argument is missing or not a positive integer
-  console.log('');
 }
