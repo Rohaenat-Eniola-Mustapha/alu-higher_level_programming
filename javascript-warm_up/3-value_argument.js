257d300420d949d045916900242de3c9a6af2bd1
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-09-26 07:17:51 -0700

    number 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,9 +3,8 @@
 // Check if the first argument exists
 if (process.argv[2] === undefined) {
   // No argument passed
-  console.log("No argument");
+  console.log('No argument');
 } else {
   // Print the first argument
   console.log(process.argv[2]);
 }
-
