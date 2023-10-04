401cc8ae29c1123788b6b689c8a30c2b232cbb4f
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-10-04 13:20:31 +0100

    task 3

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Attach a click event handler to the div with id 'red_header'
+    $('#red_header').click(function () {
+      // Add the 'red' class to the <header> element
+      $('header').addClass('red');
+    });
+  });  
